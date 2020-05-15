b5f1c3012640b03c9de665068e756a381ccaf3ac
Daniel Stoll <danielsimon.stoll2@gmail.com>
2020-05-15 12:25:25 +0200

    Enhanced setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+# Install required packages
+if type dnf &> /dev/null; then
+	sudo dnf -y install zsh
+	sudo dnf -y install rcm
+	sudo dnf -y install gnome-tweaks
+fi
+
+if type apt-get &> /dev/null; then
+	sudo apt-get -y install zsh
+	sudo apt-get -y install rcm
+	sudo apt-get -y install gnome-tweaks
+fi
+
+# Cleanup bash related config files
+rm ~/.bashrc
+rm ~/.bash_history
+rm ~/.bash_profile
+rm ~/.bash_logout
+
 # Install sdkman
-curl -s "https://get.sdkman.io" | bash
+curl -s "https://get.sdkman.io" | zsh
 
 # Create symlinks in home directory
 export RCRC=$HOME/.dotfiles/rcrc
